90b63dbcfaa56024c8cdc077df0f5aebb4874461
Emanuele Giunta <egindustries11@gmail.com>
2021-12-08 21:43:55 +0100

    Initial Commit

---
+++ Shared/newcrossdjApp.swift
@@ -0,0 +1,20 @@
+//
+//  newcrossdjApp.swift
+//  Shared
+//
+//  Created by Emanuele Giunta on 08/12/21.
+//
+
+import SwiftUI
+
+@main
+struct newcrossdjApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
